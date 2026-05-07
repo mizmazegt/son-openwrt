7412daad3888ecd4eef2b3ec2f438b1cc9ca9ad6
mizmazegt <mizmazegt@gmail.com>
2026-05-07 11:12:17 +0700

    update 25.12 openwrt

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+Tai va cai dat passwall2
+
+wget -O /tmp/luci-app-passwall2-26.5.1-r1.apk "https://github.com/Openwrt-Passwall/openwrt-passwall2/releases/download/26.5.1-1/luci-app-passwall2-26.5.1-r1.apk"
+apk add --allow-untrusted /tmp/luci-app-passwall2-26.5.1-r1.apk
+
 passwall2 version 26.5.1
 
 wget -qO- https://raw.githubusercontent.com/mizmazegt/son-openwrt/refs/heads/main/install.sh | sh
---
+++ install25.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+echo "========================================="
+echo "1. Đang chuẩn bị môi trường..."
+echo "========================================="
+# Cập nhật danh sách gói và cài đặt wget-ssl + chứng chỉ bảo mật
+
+
+# Đổi URL này cho đúng repo của bạn
+REPO_URL="https://raw.githubusercontent.com/mizmazegt/son-openwrt/refs/heads/main"
+
+FILES="
+
+usr/lib/lua/luci/view/passwall2/node_list/node_list.htm
+usr/lib/lua/luci/passwall2/util_sing-box.lua
+usr/share/passwall2/app.sh
+usr/share/passwall2/test.sh
+
+"
+
+echo "Bắt đầu tải và thiết lập cấu hình bằng WGET..."
+
+# Biến đếm số file thành công
+SUCCESS_COUNT=0
+
+for file in $FILES; do
+    # Tạo thư mục nếu chưa có
+    DIR_NAME=$(dirname "/$file")
+    mkdir -p "$DIR_NAME"
+    
+    echo "- Đang xử lý: /$file"
+    
+    # Dùng wget (đã hỗ trợ SSL):
+    # -q: im lặng, không in log rác
+    # -O: xuất ra file đích
+    wget -q -O "/$file" "$REPO_URL/$file"
+    
+    # Kiểm tra wget chạy thành công (mã 0) VÀ file không bị rỗng (-s)
+    if [ $? -eq 0 ] && [ -s "/$file" ]; then
+        echo "  -> Tải thành công!"
+        SUCCESS_COUNT=$((SUCCESS_COUNT + 1))
+        
+        # Phân quyền thông minh (Bỏ qua file .htm và .json)
+        case "$file" in
+            *.htm|*.json)
+                ;;
+            *)
+                chmod +x "/$file"
+                ;;
+        esac
+    else
+        echo "  -> [LỖI] Không thể tải file này! (Xóa file lỗi nếu có)"
+        # Xóa file trắng/lỗi do wget lỡ tạo ra để tránh rác hệ thống
+        rm -f "/$file"
+    fi
+done
+
+echo "========================================="
+echo "Hoàn tất! Đã tải thành công: $SUCCESS_COUNT file."
+echo "========================================="
+
+# Kích hoạt các dịch vụ (init.d)
+echo "Kích hoạt các dịch vụ tự chạy..."
+
+/etc/init.d/passwall2 restart
